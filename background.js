0ebd931066b0ae1edfdab7c8a256a464e2131e86
braceyourself <ethanabrace@gmail.com>
2024-06-13 12:50:07 -0400

    Correctly use chrome.scripting.executeScript to inject content script

--- background.js
+++ background.js
@@ -12,8 +12,13 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "readSelectedText") {
     chrome.storage.sync.get("openai_api_key", (data) => {
       const apiKey = data.openai_api_key;
-      console.log("Sending message to tab with API key:", apiKey);
-      chrome.tabs.sendMessage(tab.id, { action: "readSelectedText", text: info.selectionText, apiKey: apiKey });
+      console.log("Injecting content script and sending message with API key:", apiKey);
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content.js']
+      }, () => {
+        chrome.tabs.sendMessage(tab.id, { action: "readSelectedText", text: info.selectionText, apiKey: apiKey });
+      });
     });
   }
 });
