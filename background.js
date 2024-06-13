e7c444bee5d05dfec042cefb570a08aa912ff99d
braceyourself <ethanabrace@gmail.com>
2024-06-13 11:13:10 -0400

    Fix message passing

--- background.js
+++ background.js
@@ -12,13 +12,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "readSelectedText") {
     chrome.storage.sync.get("openai_api_key", (data) => {
       const apiKey = data.openai_api_key;
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: (text, apiKey) => {
-          chrome.runtime.sendMessage({ action: "readSelectedText", text, apiKey });
-        },
-        args: [info.selectionText, apiKey]
-      });
+      chrome.tabs.sendMessage(tab.id, { action: "readSelectedText", text: info.selectionText, apiKey: apiKey });
     });
   }
 });
