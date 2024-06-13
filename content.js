4dadca3a8587032862297a52d7844589e4bced20
braceyourself <ethanabrace@gmail.com>
2024-06-13 12:04:24 -0400

    Add cleaner SVG loading spinner

--- content.js
+++ content.js
@@ -15,8 +15,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       console.log("Using voice:", voice);
 
       // Create loading icon element
-      const loadingIcon = document.createElement("img");
-      loadingIcon.src = chrome.runtime.getURL("icons/loading.gif");  // Ensure loading.gif is in the icons directory
+      const loadingIcon = document.createElement("div");
+      loadingIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        <circle cx="50" cy="50" r="32" stroke-width="8" stroke="#000" stroke-dasharray="50.26548245743669 50.26548245743669" fill="none" stroke-linecap="round">
+          <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
+        </circle>
+        <circle cx="50" cy="50" r="23" stroke-width="8" stroke="#aaa" stroke-dasharray="36.12831551628262 36.12831551628262" stroke-dashoffset="36.12831551628262" fill="none" stroke-linecap="round">
+          <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" keyTimes="0;1" values="0 50 50;-360 50 50"></animateTransform>
+        </circle>
+      </svg>`;
       loadingIcon.style.position = "fixed";
       loadingIcon.style.top = "10px";
       loadingIcon.style.right = "10px";
